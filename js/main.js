687e8317ca3a4a2c8b07b6a1867d8e3509ef6d39
Istiak98 <86631588+Istiak98@users.noreply.github.com>
2022-04-01 02:39:24 +0600

    1 function added toggle

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const milestoneData = JSON.parse(data).data;
+
+//load course milestone data
+function loadMilestones() {
+  const milestones = document.querySelector(".milestones");
+
+  milestones.innerHTML = `${milestoneData
+    .map(function (milestone) {
+      return ` <div class="milestone border-b">
+    <div class="flex">
+      <div class="checkbox"><input type="checkbox" /></div>
+      <div onClick="openMilestone(this)">
+        <p>
+          ${milestone.name}
+          <span><i class="fas fa-chevron-down"></i></span>
+        </p>
+      </div>
+    </div>
+    <div class="hidden_panel">
+        ${milestone.modules
+          .map(function (module) {
+            return `<div class="module border-b">
+            <p>${module.name}</p>
+          </div>`;
+          })
+          .join("")}
+      </div>
+    </div>`;
+    })
+    .join("")}`;
+}
+
+function openMilestone(milestoneElement) {
+  const currentPanel = milestoneElement.parentNode.nextElementSibling;
+  // const shownPanel = document.querySelector(".show");
+
+  // shownPanel.classList.remove("show");
+  currentPanel.classList.toggle("show");
+}
+
+loadMilestones();
